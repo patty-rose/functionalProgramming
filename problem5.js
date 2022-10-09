adf4db8c28c7382c144a16a18330335c34e28b42
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-10-08 22:07:45 -0700

    WIP quickSort recursive for problem 5

--- problem5.js
+++ problem5.js
@@ -12,3 +12,29 @@
 // Example
 // Input: [9, 2, 7, 12]
 // Output: [2, 7, 9, 12]
+
+//quicksort:
+//declare pivot value
+  //sort into two arrays greater than and less than pivot value
+//repeat until 
+
+function quickSort(arr){
+  if (arr.length <= 1){
+    return [arr];
+  } else {
+    let lessThan = [];
+    let moreThan = [];
+    const pivot = arr[0];
+    for (let i = 0; i < arr.length; i++){
+      if(arr[i] > pivot){
+        moreThan.push(arr[i]);
+      } else if(arr[i] < pivot) {
+        lessThan.push(arr[i]);
+      }
+    }
+    return quickSort(lessThan) + pivot + quickSort(moreThan);
+  } 
+}
+
+const quickSortProblem5 = quickSort([9, 2, 7, 12, 1, 24, 18, 8, 22, 13]);
+console.log("quickSort:", quickSortProblem5);
